8120322ce996f865141b302f9e17226381e1b75f
Thomas Olache <thomas.olache@gmail.com>
2020-03-07 21:45:51 -0600

    New_files

---
+++ Libraries.R
@@ -0,0 +1,15 @@
+#Required Libraries
+
+#Excel
+library(openxlsx)
+
+#Databases
+library(RJDBC)
+
+#Formatting
+library(dplyr)
+library(lubridate)
+library(reshape2)
+
+#Machine learning
+library(h2o)
